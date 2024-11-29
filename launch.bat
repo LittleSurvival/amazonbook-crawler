1fffb1b29577528d8660f4c625cf7f14fdd4d850
TheNano <66905920+LittleSurvival@users.noreply.github.com>
2024-11-30 05:30:50 +0800

    Use venv to handle lib

--- launch.bat
+++ launch.bat
@@ -1,2 +1,8 @@
 @echo off
+:: Activate the virtual environment
+call venv\Scripts\activate
+
+:: Run the application
 python main.py
+
+pause
--- setup.bat
+++ setup.bat
@@ -1,5 +1,16 @@
 @echo off
 python -m pip install --upgrade pip
+
+:: Create the virtual environment if it doesn't exist
+if not exist venv (
+    python -m venv venv
+)
+
+:: Activate the virtual environment
+call venv\Scripts\activate
+
+:: Install dependencies
 pip install -r requirements.txt
+
 echo Libraries installed successfully.
 pause
